f54fbb6a09910a1440b40b2aadc80bbd7ff3b05f
tamybl <tamara.bravo.l@gmail.com>
2018-10-09 13:45:42 -0300

    Se agrega llamado api y ciclo de vida componentDidMount. Se renderiza data en el html

--- src/App.js
+++ src/App.js
@@ -2,29 +2,33 @@ import React, {Component} from 'react';
 import User from './components/User/User';
 import './App.css';
 
-// Obtener data mediante fetch
-const dataUsers = fetch('./db.json')
-    .then((response) => {
-        return response.json()
-    })
-    .then((data) => console.log(data));
-
 
 // Temporalmente se configura la data inicial sin llamarla del JSON.
 class App extends Component {
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
         this.state = {
-            users: [
-              { id: 1, name: "Francisco", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", photo: "https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?h=350&auto=compress&cs=tinysrgb" },
-              { id: 2, name: "Pedro", description: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.", photo: "https://images.pexels.com/photos/193355/pexels-photo-193355.jpeg?h=350&auto=compress&cs=tinysrgb"},
-              { id: 3, name: "Maria", description: "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", photo: "https://images.pexels.com/photos/157661/young-woman-shooting-model-157661.jpeg?h=350&auto=compress&cs=tinysrgb" },
-              { id: 4, name: "Jorge", description: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.", photo: "https://images.pexels.com/photos/247917/pexels-photo-247917.jpeg?h=350&auto=compress&cs=tinysrgb" },
-              { id: 5, name: "Ana", description: "Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.", photo: "https://images.pexels.com/photos/418870/pexels-photo-418870.jpeg?h=350&auto=compress&cs=tinysrgb" },
-              { id: 6, name: "Felipe", description: "Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.", photo: "https://images.pexels.com/photos/325682/pexels-photo-325682.jpeg?h=350&auto=compress&cs=tinysrgb" }
-            ]
+            users: []
           }
         }
+
+    componentDidMount() {
+        const url = "http://localhost:3000/api/";
+        let parameter = "users";
+        fetch(url + parameter)
+            .then((response) => {
+        return response.json()
+        })
+    .then((data) => {
+        let users = data.map((user, index) => {
+            return (
+                <User photo={user.photo} name={user.name} key={index}/>
+            )
+        })
+        this.setState({users: users});
+    });
+
+    }
           
         render() {
             return ( 
@@ -41,13 +45,8 @@ class App extends Component {
                             <h3>Nombre</h3>
                             <h3>Descripción</h3>
                         </div> 
-                    <User photo = "https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?h=350&auto=compress&cs=tinysrgb" name = "Francisco" />
-                    <User photo = "https://images.pexels.com/photos/193355/pexels-photo-193355.jpeg?h=350&auto=compress&cs=tinysrgb" name = "Pedro" />
-                    <User photo = "https://images.pexels.com/photos/157661/young-woman-shooting-model-157661.jpeg?h=350&auto=compress&cs=tinysrgb" name = "María" />
+                        {this.state.users}
                     </div>
-
-                    
-
 
                 </div>
             )
